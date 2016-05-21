7e1cd153b8beffd4a8da422245dc07332116bef3
Mark Raynsford <code@io7m.com>
2016-05-21 18:31:53 +0000

    Add frontend XML parser
    Additionally, remove the unused stub implementation.

--- io7m-kstructural-frontend/src/main/java/com/io7m/kstructural/frontend/KSParsers.java
+++ io7m-kstructural-frontend/src/main/java/com/io7m/kstructural/frontend/KSParsers.java
@@ -20,7 +20,6 @@ import com.io7m.jfunctional.Unit;
 import com.io7m.jlexing.core.ImmutableLexicalPosition;
 import com.io7m.jlexing.core.ImmutableLexicalPositionType;
 import com.io7m.jlexing.core.LexicalPositionType;
-import com.io7m.junreachable.UnimplementedCodeException;
 import com.io7m.junreachable.UnreachableCodeException;
 import com.io7m.kstructural.core.KSElement.KSBlock;
 import com.io7m.kstructural.core.KSParse;
@@ -47,11 +46,23 @@ import com.io7m.kstructural.parser.imperative.KSImperativeBuilderType;
 import com.io7m.kstructural.parser.imperative.KSImperativeMatch;
 import com.io7m.kstructural.parser.imperative.KSImperativeParser;
 import com.io7m.kstructural.parser.imperative.KSImperativeParserType;
+import com.io7m.kstructural.xom.KSJingValidation;
+import com.io7m.kstructural.xom.KSXOMBlockParser;
+import com.io7m.kstructural.xom.KSXOMBlockParserType;
+import com.io7m.kstructural.xom.KSXOMInlineParser;
+import com.io7m.kstructural.xom.KSXOMInlineParserType;
+import nu.xom.Builder;
+import nu.xom.Document;
+import nu.xom.ParsingException;
+import nu.xom.ValidityException;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
@@ -118,7 +129,7 @@ public final class KSParsers implements KSParserConstructorType
   public static KSParserType createXML(
     final KSParseContextType context)
   {
-    throw new UnimplementedCodeException();
+    return new XMLParser();
   }
 
   @Override
@@ -307,6 +318,80 @@ public final class KSParsers implements KSParserConstructorType
           (c, eof) -> {
             throw new UnreachableCodeException();
           });
+      }
+    }
+  }
+
+
+  private static final class XMLParser implements KSParserType
+  {
+    @Override
+    public KSResult<KSBlock<KSParse>, KSParseError> parseBlock(
+      final KSParseContextType context,
+      final Path file)
+      throws IOException
+    {
+      final KSResult<Document, KSParseError> d = XMLParser.parseDocument(file);
+      return d.flatMap(
+        document ->
+          XMLParser.validateDocument(file).flatMap(x -> {
+            final KSXOMInlineParserType ip =
+              KSXOMInlineParser.Companion.create();
+            final KSXOMBlockParserType bp =
+              KSXOMBlockParser.Companion.create(ip);
+            return bp.parse(context, document.getRootElement());
+          }));
+    }
+
+    private static KSResult<Unit, KSParseError> validateDocument(
+      final Path file)
+    {
+      try (final InputStream is = Files.newInputStream(file)) {
+        if (!KSJingValidation.validate(file, is)) {
+          final KSParseError pe =
+            new KSParseError(Optional.empty(), "Validation failed");
+          return KSResults.fail(pe);
+        }
+        return new KSSuccess<>(Unit.unit());
+      } catch (final SAXException e) {
+        final KSParseError pe =
+          new KSParseError(Optional.empty(), e.getMessage());
+        return KSResults.fail(pe);
+      } catch (final IOException e) {
+        final KSParseError pe =
+          new KSParseError(Optional.empty(), e.getMessage());
+        return KSResults.fail(pe);
+      }
+    }
+
+    private static KSResult<Document, KSParseError> parseDocument(
+      final Path file)
+      throws IOException
+    {
+      try (final InputStream is = Files.newInputStream(file)) {
+        final Builder b = new Builder();
+        final Document e = b.build(is, file.toString());
+        return new KSSuccess<>(e);
+      } catch (final ValidityException e) {
+        final FileSystem fs = file.getFileSystem();
+        final ImmutableLexicalPositionType<Path> pos =
+          ImmutableLexicalPosition.newPositionWithFile(
+            e.getLineNumber(),
+            e.getColumnNumber(),
+            fs.getPath(e.getURI()));
+        final KSParseError pe =
+          new KSParseError(Optional.of(pos), "Validation failed");
+        return KSResults.fail(pe);
+      } catch (final ParsingException e) {
+        final FileSystem fs = file.getFileSystem();
+        final ImmutableLexicalPositionType<Path> pos =
+          ImmutableLexicalPosition.newPositionWithFile(
+            e.getLineNumber(),
+            e.getColumnNumber(),
+            fs.getPath(e.getURI()));
+        final KSParseError pe =
+          new KSParseError(Optional.of(pos), "Validation failed");
+        return KSResults.fail(pe);
       }
     }
   }
